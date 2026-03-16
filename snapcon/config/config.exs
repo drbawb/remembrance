833f2fffe09cda4934d474be5e9bbd257faaefe2
Robert Straw <drbawb@fatalsyntax.com>
2026-03-16 17:03:03 -0500

    prettify the hosts table and fix CSS ref

--- snapcon/config/config.exs
+++ snapcon/config/config.exs
@@ -36,7 +36,7 @@ config :esbuild,
   version: "0.17.11",
   zfs_snapcon: [
     args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(css/app.css js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
